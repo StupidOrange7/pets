39deb30647b658ae1831050de99bd08cd3062729
Nadinegp <nadeenebrahim82@gmail.com>
2022-12-11 12:01:56 +0200

    acc

--- Account.php
+++ Account.php
@@ -2,20 +2,23 @@
 session_start();
 require_once('Config.php') ;
 ?>
+<!doctype html>
+<html>
 <link rel="stylesheet" href="main.css">
-      <header class="header" style="background-color:#efeef1; ">
-      <?php include 'header.php';
-      $email = isset($_SESSION['email']) ? $_SESSION['email'] : '';?>
+<link rel="stylesheet" href="SignIn.css">
+      <header class="header" style="background-color:#efeef1;">
+      <?php include 'header.php';?>
         </header>
-        <body>
-          <div>
-            <label style="font-size: medium;">Name: <?php echo $_SESSION["fname"];?> </label>
+        <body >
+          <div><center>
+            <label style="font-size: large;">Name: <p style="font-size: medium;"><?php echo $_SESSION["fname"];?></p> </label>
             
-            <label style="font-size: medium;">Age: <?php echo $_SESSION["age"];?></label>
+            <label style="font-size: large;">Age: <p style="font-size: medium;"><?php echo $_SESSION["age"];?></p></label>
             
-            <label style="font-size: medium;">Address: <?php echo $_SESSION["address"];?></label>
+            <label style="font-size: large;">Address:<p style="font-size: medium;"> <?php echo $_SESSION["address"];?></p></label>
             
-            <label style="font-size: medium;">Email: <?php echo $_SESSION["email"];?></label>
-    </div>
+            <label style="font-size: large;">Email: <p style="font-size: medium;"><?php echo $_SESSION["email"];?></p></label>
+            </center></div>
         </body>
         <footer><?php include 'footer.html';?></footer>
+        </html>
