e18ad74d495440a456682fe432d64531afd271f3
Nadinegp <nadeenebrahim82@gmail.com>
2022-12-11 11:42:54 +0200

    account

--- Account.php
+++ Account.php
@@ -9,9 +9,13 @@ require_once('Config.php') ;
         </header>
         <body>
           <div>
-        <?php 
-
-          	echo $email." hello ".$_SESSION["fname"]. " ".$_SESSION["address"] ;?>
+            <label style="font-size: medium;">Name: <?php echo $_SESSION["fname"];?> </label>
+            
+            <label style="font-size: medium;">Age: <?php echo $_SESSION["age"];?></label>
+            
+            <label style="font-size: medium;">Address: <?php echo $_SESSION["address"];?></label>
+            
+            <label style="font-size: medium;">Email: <?php echo $_SESSION["email"];?></label>
     </div>
         </body>
         <footer><?php include 'footer.html';?></footer>
